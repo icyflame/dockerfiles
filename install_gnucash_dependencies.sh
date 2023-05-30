5e414c5adc4a9edbf077a1cd5823c54b484f5f0b
dave <dave>
2023-05-29 17:30:28 -0700

    formatting

--- install_gnucash_dependencies.sh
+++ install_gnucash_dependencies.sh
@@ -4,30 +4,30 @@ apt update -y
 apt upgrade -y 
 
 apt install -y  \
-		git \
-		gettext \
-		wget \
-		libglib2.0-dev \
-		pkg-config \
-		cmake \
-		gcc \
-		g++ \
-		libxml2-dev \
-		libxslt1-dev \
-		xsltproc \
-		swig \
-		guile-2.2-dev \
-		gwenhywfar-tools \
-		libofx-dev \
-		libgtest-dev \
-		libdbi-dev \
-		libxml2-utils \
-        libwebkit2gtk-4.1-dev \
-		libboost-dev \
-		libboost-date-time-dev \
-		libboost-filesystem-dev \
-		libboost-locale-dev \
-		libboost-regex-dev \
-		libboost-program-options-dev \
-		libboost-system-dev 
+    git \
+    gettext \
+    wget \
+    libglib2.0-dev \
+    pkg-config \
+    cmake \
+    gcc \
+    g++ \
+    libxml2-dev \
+    libxslt1-dev \
+    xsltproc \
+    swig \
+    guile-2.2-dev \
+    gwenhywfar-tools \
+    libofx-dev \
+    libgtest-dev \
+    libdbi-dev \
+    libxml2-utils \
+    libwebkit2gtk-4.1-dev \
+    libboost-dev \
+    libboost-date-time-dev \
+    libboost-filesystem-dev \
+    libboost-locale-dev \
+    libboost-regex-dev \
+    libboost-program-options-dev \
+    libboost-system-dev 
 
